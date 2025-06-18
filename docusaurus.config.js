b3796751871e508626961658b22d52fce0edd184
advisorpages <advisorpagesteam@gmail.com>
2025-06-18 14:07:10 -0400

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,5 @@
+url: 'https://advisorpages.github.io',
+baseUrl: '/training/',
+
+organizationName: 'advisorpages',     // GitHub org or user name
+projectName: 'training',              // Repo name
