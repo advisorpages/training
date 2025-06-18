7b35631ecd65cdb77b10a9e45d4a83823d58514d
advisorpages <advisorpagesteam@gmail.com>
2025-06-18 14:29:14 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,31 @@
-url: 'https://advisorpages.github.io',
-baseUrl: '/training/',
+// docusaurus.config.js
 
-organizationName: 'advisorpages',
-projectName: 'training',
+module.exports = {
+  title: 'Training',
+  tagline: 'Internal team training resources',
+  url: 'https://advisorpages.github.io',
+  baseUrl: '/training/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  organizationName: 'advisorpages', // GitHub org/user
+  projectName: 'training',          // GitHub repo name
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        pages: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
