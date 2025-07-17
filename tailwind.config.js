3d6a467103d9d8ae9bcee75f0448dab11ffd0e34
advisorpages <advisorpagesteam@gmail.com>
2025-07-17 01:52:23 -0400

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.md",
-    "./*.html",
+    "./*.{md,html,njk}",
     "./_includes/**/*.{html,njk}",
-    "./_site/**/*.html"
+    "./_data/**/*.{json,js}",
+    "./_site/**/*.html" // Optional: only needed if rendering happens in post-processing
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), // 👈 add this if you're using `.prose`
+  ],
 }
