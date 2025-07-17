dcdb270687dd782440e501524391bc6a83629f37
advisorpages <advisorpagesteam@gmail.com>
2025-07-17 00:32:28 -0400

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.md",
+    "./*.html",
+    "./_includes/**/*.{html,njk}",
+    "./_site/**/*.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
